a30fcbab71bbc4ad320769a2b21722cf1eed010c
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-05-18 14:22:09 +0600

    express body get

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,27 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
 const port = 3000;
 
-app.get("/user", (req, res) => {
-  res.send("Express Server");
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+// Express Server
+app.get("/", (req: Request, res: Response) => {
+  //   res.send("Express Server");
+  res.status(200).json({
+    message: "Express Server",
+    authors: "Hasnath Ahmed",
+  });
+});
+
+// Create User
+app.post("/users", async (req: Request, res: Response) => {
+  const result = req.body; // Assuming the user data is sent in the request body
+  console.log(result);
+  // Implementation for creating a user
 });
 
 app.listen(port, () => {
